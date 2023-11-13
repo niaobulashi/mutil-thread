a2be01f48e1091c142bea0df51e016bee1b6e262
hulang <hulang6666@qq.com>
2023-11-13 17:00:37 +0800

    init project

---
+++ src/main/java/com/hulang/mutilthread/MutilThreadApplication.java
@@ -0,0 +1,13 @@
+package com.hulang.mutilthread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutilThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutilThreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hulang/mutilthread/MutilThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulang.mutilthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutilThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
